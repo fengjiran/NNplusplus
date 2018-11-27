801a7ebf9db44130cca2f6ad76aa41a21b24e18f
fengjiran <fengjiran@foxmail.com>
2018-11-27 10:14:54 +0800

    add ators

--- matrix.hpp
+++ matrix.hpp
@@ -17,8 +17,25 @@
 
 class Matrix {
 private:
-    size_t m_size_;  // typedef unsigned long long int size_t
-    size_t n_size_;
+    size_t m_size_;  // typedef unsigned long long int size_t, (M)xN
+    size_t n_size_;  // Mx(N)
+    double *matrix_;  // A pointer to the array.
+    double **row_ptrs_; // An array of row pointers.
+    // used to avoid repeated arithmetics at each access to the matrix.
+public:
+    /**********************************************************
+     * Constructors
+     **********************************************************/
+
+    // Basic ctor to initialize a matrix of size m by n.
+    // All matrix positions will be initialized to 0.
+    Matrix(size_t m = 0, size_t n = 0);
+
+    // copy ctor
+    Matrix(const Matrix &rhs);
+
+    // copy assignment operator
+    Matrix &operator=(const Matrix &rhs);
 
 };
 
