4e21f0648f7f5f209c23b9751842f6cf9b03790b
fengjiran <fengjiran@foxmail.com>
2018-11-27 09:22:29 +0800

    add includes

--- matrix.hpp
+++ matrix.hpp
@@ -2,7 +2,15 @@
 // Created by Richard on 2018/11/27.
 //
 
+//  A matrix object, which includes basic operations such as
+//  matrix transpose and dot product.
+
 #ifndef NNPLUSPLUS_MATRIX_HPP
 #define NNPLUSPLUS_MATRIX_HPP
+
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
 
 #endif //NNPLUSPLUS_MATRIX_HPP
