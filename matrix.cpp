7b84599015a8078e78af9b91882347628c74f9c3
fengjiran <fengjiran@foxmail.com>
2018-11-28 16:13:21 +0800

    add ctor2

--- matrix.cpp
+++ matrix.cpp
@@ -4,7 +4,8 @@
 
 #include "matrix.hpp"
 
-Matrix::Matrix(size_t m, size_t n) : m_size_(m), n_size_(n) {
+Matrix::Matrix(size_t m, size_t n)
+        : m_size_(m), n_size_(n) {
     matrix_ = new double[m_size_ * n_size_]();  // initialized to 0
     row_ptrs_ = new double *[m_size_];
 
@@ -12,3 +13,14 @@ Matrix::Matrix(size_t m, size_t n) : m_size_(m), n_size_(n) {
         row_ptrs_[i] = &matrix_[i * n_size_];
 
 }
+
+Matrix::Matrix(const Matrix &rhs)
+        : m_size_(rhs.m_size_), n_size_(rhs.n_size_) {
+    matrix_ = new double[m_size_ * n_size_]();
+    row_ptrs_ = new double *[m_size_];
+
+    for (size_t i = 0; i < m_size_ * n_size_; i++)
+        matrix_[i] = rhs.matrix_[i];
+}
+
+Matrix Matrix::dot(const Matrix &rhs) const {}
